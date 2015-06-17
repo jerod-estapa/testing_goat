858104c959a881057180e3a2958ae8215e4759bb
jerodestapa <jerodestapa@gmail.com>
2015-06-16 23:39:53 -0500

    Changed functional_tests.py to use unittest

--- functional_tests.py
+++ functional_tests.py
@@ -3,33 +3,42 @@
 from selenium import webdriver
 import unittest
 
-browser = webdriver.Firefox()
+class NewVisitorTest(unittest.TestCase):
 
-# Margot has heard about a new to-do app online and goes to
-# the URL to check it out.
-browser.get('http://localhost:8000')
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-# She notices the page title and header mention to-do lists.
-assert 'To-Do' in browser.title
+    def tearDown(self):
+        self.browser.quit()
 
-# She is invited to enter a to-do item straight away.
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Margot has heard about a new to-do app online and goes to
+        # the URL to check it out.
+        self.browser.get('http://localhost:8000')
 
-# She types "Buy new football" into the text box (Margot loves sports).
+        # She notices the page title and header mention to-do lists.
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
 
-# When she hits enter, the page updates, and now the page lists,
-# "#1: Buy new football" as an item in a to-do list.
+        # She is invited to enter a to-do item straight away.
 
-# There is still a text box asking her to enter another item. She enters,
-# "Buy a soccer ball, too" (Margot really loves sports).
+        # She types "Buy new football" into the text box (Margot loves sports).
 
-# The page updates again, and now shows both items on her list.
+        # When she hits enter, the page updates, and now the page lists,
+        # "#1: Buy new football" as an item in a to-do list.
 
-# Margot wonders if the site will remember her list. Then, she sees
-# that the site has generated a unique URL for her -- there is some
-# explanatory text to that effect.
+        # There is still a text box asking her to enter another item. She enters,
+        # "Buy a soccer ball, too" (Margot really loves sports).
 
-# She visits the URL - her to-do list is still there.
+        # The page updates again, and now shows both items on her list.
 
-# Satisfied, she goes back to sleep
+        # Margot wonders if the site will remember her list. Then, she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
 
-browser.quit()
+        # She visits the URL - her to-do list is still there.
+
+        # Satisfied, she goes back to sleep
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
